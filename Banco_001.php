606636c568805d5ec9d1c08ff22c8d45fa2621a7
Francisco Luz <franciscoferreiraluz@yahoo.com.au>
2011-07-19 22:58:48 -0400

    Compatibility with PHP 5.2 #2

--- Banco_001.php
+++ Banco_001.php
@@ -30,39 +30,41 @@
  *    
  */
 class Banco_001 extends Boleto{
-    function setUp(){
-        $this->bank_name  = 'Banco do Brasil SA';
+    function setUp($boleto){
+        $boleto->bank_name  = 'Banco do Brasil SA';
     }
     
     //Implementation of Febraban free range set from position 20 to 44
-    function febraban_20to44(){
+    function febraban_20to44($boleto){
        /** get convenio number (convention) and contract number **/
 	//set empty variable to avoid any eventuality
 	$convenio     = '';
 	$nosso_numero = '';
 	
 	//see readme.txt
-	$cc = explode('-', $this->arguments['carteira_nosso_numero']);
+	$cc = explode('-', $boleto->arguments['carteira_nosso_numero']);
 	
-	$convenio = $this->computed['convenio'] = $cc[0];
-	$contrato = $this->computed['contrato'] = '';
-	$servico  = $this->computed['servico']  = '';
+	$convenio = $boleto->computed['convenio'] = $cc[0];
+	$contrato = $boleto->computed['contrato'] = '';
+	$servico  = $boleto->computed['servico']  = '';
 	if(isset($cc[1])){
-	   $contrato = $this->computed['contrato'] = $cc[1]; 
+	   $contrato = $boleto->computed['contrato'] = $cc[1]; 
 	}
 	if(isset($cc[2])){
-	   $servico = $this->computed['servico'] = $cc[2];
+	   $servico = $boleto->computed['servico'] = $cc[2];
 	}
 
 	//Carteira might come in this format xx-xxx so we gotta break it apart.
-	$carteira     = explode('-', $this->arguments['carteira']);
-	$carteira_sub = $this->computed['carteira_sub'] = '';
+	$carteira     = explode('-', $boleto->arguments['carteira']);
+	$carteira_sub = $boleto->computed['carteira_sub'] = '';
         if(isset($carteira[1])){
-	     $carteira_sub = $this->computed['carteira_sub'] = $carteira[1];
+	     $carteira_sub = $boleto->computed['carteira_sub'] = $carteira[1];
 	}
 	//pre calculate nosso_numero check digit
-	$checkDigit = $this->modulo_11($convenio.$this->arguments['nosso_numero']);
+	$checkDigit = $boleto->modulo_11($convenio.$boleto->arguments['nosso_numero']);
 	$checkDigit['digito'] = '-'.$checkDigit['digito'];
+	
+	$code = '';
 	
 	switch($carteira[0]){
 	   case 18:
@@ -74,7 +76,7 @@ class Banco_001 extends Boleto{
 		  // 34-42 -> Nosso Número (sem dígito)   9
 		  // 43-44 -> Carteira                    2
 		     $convenio     = str_pad($convenio, 14, 0, STR_PAD_LEFT);
-		     $nosso_numero = str_pad($this->arguments['nosso_numero'], 9, 0, STR_PAD_LEFT);
+		     $nosso_numero = str_pad($boleto->arguments['nosso_numero'], 9, 0, STR_PAD_LEFT);
 		     
 		     //25 digits long code
 		     $code         = $convenio.$nosso_numero.$carteira[0];
@@ -84,7 +86,7 @@ class Banco_001 extends Boleto{
 		  // 33-42 -> Nosso Número (sem dígito)  10
 		  // 43-44 -> Carteira                    2
 		     $convenio     = str_pad($convenio, 13, 0, STR_PAD_LEFT);
-		     $nosso_numero = str_pad($this->arguments['nosso_numero'], 10, 0, STR_PAD_LEFT);
+		     $nosso_numero = str_pad($boleto->arguments['nosso_numero'], 10, 0, STR_PAD_LEFT);
 		     
 		     //25 digits long code
 		     $code  = $convenio.$nosso_numero.$carteira[0];
@@ -99,7 +101,7 @@ class Banco_001 extends Boleto{
 		      // 26-42 -> Nosso Número (sem dígito)  17
 		      // 43-44 -> Servico                    2
 			 $convenio     = str_pad($convenio, 6, 0, STR_PAD_LEFT);
-			 $nosso_numero = str_pad($this->arguments['nosso_numero'], 17, 0, STR_PAD_LEFT);
+			 $nosso_numero = str_pad($boleto->arguments['nosso_numero'], 17, 0, STR_PAD_LEFT);
 			 
 			 //25 digits long code
 			 $code  = $convenio.$nosso_numero.$servico;
@@ -110,10 +112,10 @@ class Banco_001 extends Boleto{
 		      // 35-42 -> Conta                      8
 		      // 43-44 -> Carteira                   2
 			 $convenio     = str_pad($convenio, 6, 0, STR_PAD_LEFT);
-			 $nosso_numero = str_pad($this->arguments['nosso_numero'], 5, 0, STR_PAD_LEFT);
+			 $nosso_numero = str_pad($boleto->arguments['nosso_numero'], 5, 0, STR_PAD_LEFT);
 			 
 			 //25 digits long code
-			 $code  = $convenio.$nosso_numero.$this->arguments['agencia'].$this->arguments['conta'].$carteira[0];		         
+			 $code  = $convenio.$nosso_numero.$boleto->arguments['agencia'].$boleto->arguments['conta'].$carteira[0];		         
 		     }
 		break;
 	      }
@@ -125,28 +127,28 @@ class Banco_001 extends Boleto{
 	  
 	}
        //positons 20 to 44
-       $this->febraban['20-44'] = $code;
+       $boleto->febraban['20-44'] = $code;
        
        //save nosso_numero	
-       $this->computed['nosso_numero'] = $convenio.$this->arguments['nosso_numero'].$checkDigit['digito'];
+       $boleto->computed['nosso_numero'] = $convenio.$boleto->arguments['nosso_numero'].$checkDigit['digito'];
 	
     }
 
     //customize object to meet specific needs
-    function custom(){
+    function custom($boleto){
         //calculates check digit for branch number
-        $this->computed['agencia_dv'] = $this->arguments['agencia_dv'];
-        if(empty($this->arguments['agencia_dv']) &&  $this->arguments['agencia_dv'] != '0'){
-            $agencia_dv = $this->modulo_11($this->arguments['agencia']);
-            $this->computed['agencia_dv'] = $agencia_dv['digito'];
+        $boleto->computed['agencia_dv'] = $boleto->arguments['agencia_dv'];
+        if(empty($boleto->arguments['agencia_dv']) &&  $boleto->arguments['agencia_dv'] != '0'){
+            $agencia_dv = $boleto->modulo_11($boleto->arguments['agencia']);
+            $boleto->computed['agencia_dv'] = $agencia_dv['digito'];
         }
     }
 
     //manipulate output fields before them getting rendered. This method is called by output().
-    public function outputValues(){
-        $this->output['agencia_codigo_cedente'] = $this->arguments['agencia'].'-'.$this->computed['agencia_dv'].' / '.$this->arguments['conta'].'-'.$this->arguments['conta_dv'];
+    public function outputValues($boleto){
+        $boleto->output['agencia_codigo_cedente'] = $boleto->arguments['agencia'].'-'.$boleto->computed['agencia_dv'].' / '.$boleto->arguments['conta'].'-'.$boleto->arguments['conta_dv'];
         
-        $this->output['contrato']   = $this->computed['contrato'];
+        $boleto->output['contrato']   = $boleto->computed['contrato'];
     }
 }
 ?>
